7b8899d863127bf70e1468f18a83e7a07eed84e7
Enio Nicael <enionicael@gmail.com>
2020-11-26 18:58:43 -0300

    Exercicio cria um array com valores de 1 a 25

---
+++ bloco_4/intro-javascript-e-logica-de-programacao/dia_2/criaArray.js
@@ -0,0 +1,11 @@
+/**
+ * Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+ */
+
+let meuArry = [];
+
+for (let index = 0; index < 25; index++) {
+  meuArry.push(index + 1);
+}
+
+console.log(meuArry);
