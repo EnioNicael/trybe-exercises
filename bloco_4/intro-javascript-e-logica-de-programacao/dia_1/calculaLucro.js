b8936c06b7ad8ba28c501f12fe78f8aa75323199
Enio Nicael <enionicael@gmail.com>
2020-11-26 23:43:23 -0300

    Exercicio calcula lucro de venda

--- bloco_4/intro-javascript-e-logica-de-programacao/dia_1/calculaLucro.js
+++ bloco_4/intro-javascript-e-logica-de-programacao/dia_1/calculaLucro.js
@@ -2,11 +2,16 @@
  * Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
  */
 
-let custoDoProduto = 0;
-let valorDeVendaProduto = 0;
+let custoDoProduto = 100;
+let valorDeVendaProduto = 142;
 
-if (custoDoProduto < 0 && valorDeVendaProduto < 0) {
+if (custoDoProduto < 0 || valorDeVendaProduto < 0) {
   console.log('Erro!');
 } else {
-  let imposto = 
+  let imposto = (custoDoProduto * 20) / 100;
+  let valorCustoTotal = custoDoProduto + imposto;
+
+  let lucro = (valorDeVendaProduto - valorCustoTotal) * 1000;
+
+  console.log(lucro);
 }
