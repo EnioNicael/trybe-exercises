1f34952583c81e3b068f8c85cb9290a5d3da2859
Enio Nicael <enionicael@gmail.com>
2021-01-12 18:11:48 -0300

    exercicio1 trasforma concatenacao de strings em template literals.

--- bloco_7/introducao-a-es6_1/exercise1.js
+++ bloco_7/introducao-a-es6_1/exercise1.js
@@ -1,13 +1,12 @@
 const testingScope = (escopo) => { 
   if (escopo === true) { 
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     console.log(ifScope);
   } else {
     const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-  // console.log(ifScope + ' o que estou fazendo aqui ? :O');
 }
 
 testingScope(true);
