b4345d330c6f0c2801f4b9d5d6ca54b9e67e6374
Enio Nicael <enionicael@gmail.com>
2021-01-12 18:25:28 -0300

    Exercicio2 cria uma funcao que retorna um array ordenado.

--- bloco_7/introducao-a-es6_1/exercise2.js
+++ bloco_7/introducao-a-es6_1/exercise2.js
@@ -2,4 +2,10 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
 
+const sortOddsAndEvens = () => {
+  const orderedArray = oddsAndEvens.sort((a, b) => a - b);
+  return orderedArray;
+};
+
 console.log(oddsAndEvens);
+console.log(sortOddsAndEvens());
