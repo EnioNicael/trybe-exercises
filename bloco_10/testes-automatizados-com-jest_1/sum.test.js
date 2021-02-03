1d5973e2a0a66e354e3f6729ebe97edf3b225e39
Enio Nicael <enionicael@gmail.com>
2021-02-03 19:18:11 -0300

    Test se o retorno de sum(4, 5) é 9.

--- bloco_10/testes-automatizados-com-jest_1/sum.test.js
+++ bloco_10/testes-automatizados-com-jest_1/sum.test.js
@@ -1,5 +1,10 @@
+const { test, expect } = require('@jest/globals');
 const sum = require('./sum');
 
 test('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
   expect(sum(2, 3)).toBe(5);
+});
+
+test('Teste se o retorno de sum(4, 5) é 9', () => {
+  expect(sum(4, 5)).toBe(9);
 });
