ff4987b8d19b5b00e451bdbfccd60e44391ebd4e
Enio Nicael <enionicael@gmail.com>
2021-02-03 19:20:23 -0300

    Teste se o retorno de sum(0, 0) é 0

--- bloco_10/testes-automatizados-com-jest_1/sum.test.js
+++ bloco_10/testes-automatizados-com-jest_1/sum.test.js
@@ -8,3 +8,7 @@ test('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
 test('Teste se o retorno de sum(4, 5) é 9', () => {
   expect(sum(4, 5)).toBe(9);
 });
+
+test('Teste se o retorno de sum(0, 0) é 0', () => {
+  expect(sum(0, 0)).toBe(0);
+});
