455e70234fec94001b69fef2edb1d0318e922a83
Enio Nicael <enionicael@gmail.com>
2021-02-04 23:38:03 -0300

    Exercicio 2 implementa um teste para um caso de reject de uma promise.

--- bloco_10/testes-automatizados-com-jest_2/exercicio-2.test.js
+++ bloco_10/testes-automatizados-com-jest_2/exercicio-2.test.js
@@ -1,10 +1,17 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
+const { error } = require('console');
 const getUserName = require('./exercicio-2');
 
 describe('Testa busca de dados com Promises', () => {
-  test('Testa se um usuario foi encontrado.', () => {
+  it('Testa se um usuario foi encontrado.', () => {
     return getUserName(4).then(data => {
       expect(data).toBe('Mark');
     });
+  });
+  it('Testa o caso em que o usuário não é encontrado', () => {
+    expect.assertions(1);
+    return getUserName(2).catch(error => {
+      expect(error).toEqual({ error: 'User with 2 not found.' })
+    })
   });
 });
