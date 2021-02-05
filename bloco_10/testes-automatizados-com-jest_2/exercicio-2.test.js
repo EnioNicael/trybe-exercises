abb4070cd002699ef71e9c6b6b75f2c885799d3f
Enio Nicael <enionicael@gmail.com>
2021-02-05 00:02:09 -0300

    Exercicio 2 implementa um teste para funcao assincrona usando async/await.

--- bloco_10/testes-automatizados-com-jest_2/exercicio-2.test.js
+++ bloco_10/testes-automatizados-com-jest_2/exercicio-2.test.js
@@ -1,5 +1,3 @@
-const { expect, test } = require('@jest/globals');
-const { error } = require('console');
 const getUserName = require('./exercicio-2');
 
 describe('Testa busca de dados com Promises', () => {
@@ -13,5 +11,17 @@ describe('Testa busca de dados com Promises', () => {
     return getUserName(2).catch(error => {
       expect(error).toEqual({ error: 'User with 2 not found.' })
     })
+  });
+  it('Testa busca de dados usando async/await em caso de', async () => {
+    const data = await getUserName(4);
+    expect(data).toBe('Mark');
+  });
+  it('Testa busca de dados usando async/await em caso de erro', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(2);
+    } catch (error) {
+      expect(error).toEqual({ error: 'User with 2 not found.' })
+    }
   });
 });
